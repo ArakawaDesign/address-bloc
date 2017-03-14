b0b312061ba2c9c9659a8bc751dd43d14fb68d2d
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-03-14 15:40:14 +0000

    Checkpoint 9 Assignment: Completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@ class MenuController
         puts "2 - Create an entry"
         puts "3 - Search for an entry"
         puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "5 - Destroy all entries"
+        puts "6 - Exit"
         print "Enter your selection: "
  
         selection = gets.to_i
@@ -37,6 +38,11 @@ class MenuController
                 read_csv
                 main_menu
             when 5
+                system "clear"
+                @address_book.destroy
+                puts "You have destroyed all entries"
+                main_menu
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
